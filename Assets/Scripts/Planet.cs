c17cecb0f7aa3ae20d42cd81434e934eecf68f6b
MeguminFireFox <ambrehardy013@gmail.com>
2024-03-12 10:02:30 +0100

    des trucs

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    private float currenttime = 0;
+    private float speed = 3;
+
+    void FixedUpdate()
+    {
+        currenttime += Time.deltaTime;
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (currenttime > 30)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
